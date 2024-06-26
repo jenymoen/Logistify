4dd22a8a3ad6c0c7bcda20d7ca2c2da04bdde94f
jenymoen <61492882+jenymoen@users.noreply.github.com>
2024-06-26 19:38:28 +0000

    first commit

--- logistify/admin.py
+++ logistify/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Shipment)
 
 # Register your models here.
--- logistify/models.py
+++ logistify/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.enums import Choices
 
-# Create your models here.
+
+class Shipment(models.Model):
+  shipment_name = models.CharField(max_length=100, null=True, blank=True)
+  shipment_description = models.CharField(max_length=100, null=True, blank=True)
+  shipment_status = models.CharField(max_length=100, null=True, blank=True)
+  shipment_date = models.DateField(null=True, blank=True)
+  shipment_time = models.TimeField(null=True, blank=True)
+  shipment_weight = models.FloatField(null=True, blank=True)
+  shipment_volume = models.FloatField(null=True, blank=True)
+  shipment_value = models.FloatField(null=True, blank=True)
+  shipment_cost = models.FloatField(null=True, blank=True)
+  shipment_origin = models.CharField(max_length=100, null=True, blank=True)
+  expected_delivery_date = models.DateField(null=True, blank=True)
+
+
+
+class Supplier(models.Model):
+  supplier_name = models.CharField(max_length=100, null=True, blank=True)
+  
